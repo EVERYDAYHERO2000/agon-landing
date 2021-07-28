700c3403bc7f7a9aee26336805f20aa8cda399cb
Ilya Aralin <ilyaflare@gmail.com>
2021-07-28 21:01:58 +0300

    fix mobile cards

--- src/partials/feature/feature.js
+++ src/partials/feature/feature.js
@@ -1,6 +1,32 @@
 export const renderFeature = () => {
-    const $features = document.querySelectorAll('.feature');
-    /*
+  const $features = document.querySelectorAll(".feature");
+
+  window.addEventListener("scroll", (e) => {
+    if (window.innerWidth <= 580) {
+      for (let i = 0; i < $features.length; i++) {
+        const $currentFeature = $features[i];
+        const $nextFeature = $features[i + 1] || null;
+        const $currentScreen = $currentFeature.querySelector(".screen");
+        const currentBBtop = $features[i].getBoundingClientRect().top;
+        const nextBBtop = $nextFeature
+          ? $nextFeature.getBoundingClientRect().top
+          : null;
+        const windowHeight = window.innerHeight;
+
+        if ($nextFeature) {
+          if (nextBBtop < windowHeight) {
+            $currentScreen.classList.add("screen__end-scroll");
+          } else {
+            $currentScreen.classList.remove("screen__end-scroll");
+          }
+        }
+        //const nextBB = ($features[i+1]) ? $features[i+1].getBoundingClientRect() : 0;
+        //if (nextBB.top > 0) console.log(nextBB.top, $features[i+1])
+      }
+    }
+  });
+
+  /*
     for (let $f of $features) {
         const $card = $f.querySelector('.card');
         const color = window.getComputedStyle($f).backgroundColor;
@@ -18,4 +44,4 @@ export const renderFeature = () => {
         });
     }
     */
-}
+};

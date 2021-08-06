52b267e97929f98cd3e89c13359a0c662e90198a
Ilya Aralin <ilyaflare@gmail.com>
2021-08-06 23:54:56 +0300

    fix

--- src/partials/feature/feature.js
+++ src/partials/feature/feature.js
@@ -7,7 +7,6 @@ export const renderFeature = () => {
         const $currentFeature = $features[i];
         const $nextFeature = $features[i + 1] || null;
         const $currentScreen = $currentFeature.querySelector(".screen");
-        const currentBBtop = $features[i].getBoundingClientRect().top;
         const nextBBtop = $nextFeature
           ? $nextFeature.getBoundingClientRect().top
           : null;
@@ -20,28 +19,7 @@ export const renderFeature = () => {
             $currentScreen.classList.remove("screen__end-scroll");
           }
         }
-        //const nextBB = ($features[i+1]) ? $features[i+1].getBoundingClientRect() : 0;
-        //if (nextBB.top > 0) console.log(nextBB.top, $features[i+1])
       }
     }
   });
-
-  /*
-    for (let $f of $features) {
-        const $card = $f.querySelector('.card');
-        const color = window.getComputedStyle($f).backgroundColor;
-
-        const $shadow = document.createElement('div');
-        $shadow.classList.add('card__shadow');
-
-        $card.append($shadow);
-
-        $card.addEventListener('mousemove', (e)=> {
-            
-                $shadow.style.top = e.layerY + 'px';
-                $shadow.style.left = e.layerX + 'px';
-            
-        });
-    }
-    */
 };
